49b6fb5556dc1fe1da712acd3b916af89a5658e3
Jorge Gonzalez <jorgeag2004@icloud.com>
2025-12-16 18:06:52 -0500

    impl delete_study_item_by_id

--- src/lib/data/delete_study_item_by_id.tsx
+++ src/lib/data/delete_study_item_by_id.tsx
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { eq } from 'drizzle-orm';
 import {study_items_table} from "@/db/schema";
 
-export async function delete_assignment_by_id(id: string): Promise<void> {
+export async function delete_study_item_by_id(id: string): Promise<void> {
 
     const db = drizzle(process.env.DATABASE_URL!)
 
