d765405018c972109822e19113e0bf962c29cd9b
Jorge Gonzalez <jorgeag2004@icloud.com>
2025-12-16 20:58:56 -0500

    refactored edit_learn_item_by_id to allow optional a specific changes

--- src/lib/data/edit_learn_item_by_id.tsx
+++ src/lib/data/edit_learn_item_by_id.tsx
@@ -4,10 +4,21 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { eq } from 'drizzle-orm';
 import {learn_items_table} from "@/db/schema";
 
-export async function update_learn_item_by_id(learn_item: LearnItem): Promise<void> {
+interface update_learn_item_props {
+    id: string;
+    name?: string;
+    date_covered?: string;
+}
+
+export async function update_learn_item_by_id({id, name, date_covered}: update_learn_item_props): Promise<void> {
     const db = drizzle(process.env.DATABASE_URL!)
 
-    await db.update(learn_items_table)
-        .set({name: learn_item.name, date_covered: learn_item.date_covered})
-        .where(eq(learn_items_table.id, learn_item.id));
+    if (name) {
+        await db.update(learn_items_table)
+            .set({name: name}).where(eq(learn_items_table.id, id))
+    }
+    if (date_covered) {
+        await db.update(learn_items_table)
+            .set({date_covered: date_covered}).where(eq(learn_items_table.id, id))
+    }
 }
