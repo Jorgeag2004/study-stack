3b28ae640d0331ec3f4695d014b6e51d4cd4fb53
Jorge Gonzalez <jorgeag2004@icloud.com>
2025-12-16 21:05:55 -0500

    refactored edit_course_by_id to allow optional and specific changes

--- src/lib/data/edit_course_by_id.tsx
+++ src/lib/data/edit_course_by_id.tsx
@@ -1,13 +1,22 @@
-import { Course } from '@/types/course'
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { eq } from 'drizzle-orm';
 import { courses_table } from "@/db/schema";
+interface edit_course_props {
+    id: string;
+    name?: string;
+    icon?: string;
+}
 
-export async function edit_course_by_id(course: Course): Promise<void> {
+export async function edit_course_by_id({id, name, icon}: edit_course_props): Promise<void> {
     const db = drizzle(process.env.DATABASE_URL!)
 
-    await db.update(courses_table)
-        .set({name: course.name, icon: course.icon})
-        .where(eq(courses_table.id, course.id))
+    if (name) {
+        db.update(courses_table)
+            .set({name: name}).where(eq(courses_table.id, id))
+    }
+    if (icon) {
+        db.update(courses_table)
+            .set({icon: icon}).where(eq(courses_table.id, id))
+    }
 }
