2d2a51de696680b673397222c5d92d7e6646dc85
Jorge Gonzalez <jorgeag2004@icloud.com>
2025-12-16 20:59:58 -0500

    fixed interface name in edit_learn_item_by_id.tsx

--- src/lib/data/edit_learn_item_by_id.tsx
+++ src/lib/data/edit_learn_item_by_id.tsx
@@ -4,13 +4,13 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { eq } from 'drizzle-orm';
 import {learn_items_table} from "@/db/schema";
 
-interface update_learn_item_props {
+interface edit_learn_item_props {
     id: string;
     name?: string;
     date_covered?: string;
 }
 
-export async function update_learn_item_by_id({id, name, date_covered}: update_learn_item_props): Promise<void> {
+export async function edit_learn_item_by_id({id, name, date_covered}: edit_learn_item_props): Promise<void> {
     const db = drizzle(process.env.DATABASE_URL!)
 
     if (name) {
