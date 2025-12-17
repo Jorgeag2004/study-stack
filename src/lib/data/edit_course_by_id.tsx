2dace55b45762501e77434785c346763e8a15fbc
Jorge Gonzalez <jorgeag2004@icloud.com>
2025-12-16 21:15:03 -0500

    refactored edit_course_by_id to sync course name change with associated assignments, study_items, and learn_items

--- src/lib/data/edit_course_by_id.tsx
+++ src/lib/data/edit_course_by_id.tsx
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { eq } from 'drizzle-orm';
-import { courses_table } from "@/db/schema";
+import { courses_table, assignments_table, learn_items_table, study_items_table } from "@/db/schema";
 interface edit_course_props {
     id: string;
     name?: string;
@@ -14,6 +14,13 @@ export async function edit_course_by_id({id, name, icon}: edit_course_props): Pr
     if (name) {
         await db.update(courses_table)
             .set({name: name}).where(eq(courses_table.id, id))
+
+        await db.update(assignments_table)
+            .set({course_name: name}).where(eq(assignments_table.course_id, id))
+        await db.update(learn_items_table)
+            .set({course_name: name}).where(eq(learn_items_table.course_id, id))
+        await db.update(study_items_table)
+            .set({course_name: name}).where(eq(study_items_table.course_id, id))
     }
     if (icon) {
         await db.update(courses_table)
