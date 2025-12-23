cf48b0ae1ba66185c30ab04f2ea65636ab2c6129
Jorge Gonzalez <jorgeag2004@icloud.com>
2025-12-22 21:09:44 -0500

    styled home link

--- src/components/NavBarContent.tsx
+++ src/components/NavBarContent.tsx
@@ -5,15 +5,19 @@ import {CourseButton} from "@/components/buttons/CourseButton";
 import {CirclePlus} from "lucide-react";
 import {useState} from "react";
 import {CourseForm} from "@/components/forms/CourseForm";
+import {Home} from 'lucide-react'
 
 export const NavbarContent = ({courses}: {courses: Course[]}) => {
     const [openAddCourse, setOpenAddCourse] = useState(false);
 
     return (
         <div className="flex flex-col gap-8 w-[15%] h-[100%] min-w-[250px] max-w-[350px] bg-neutral-800 border-r border-green-500">
-            <Link className={'pl-3'} href="/">
-                <p>home</p>
+            <Link className={'pl-3 pt-4'} href={'/'}>
+                <div className={'flex flex-cols gap-2'}>
+                    <p className={'font-bold text-green-600 text-2xl'}>StudyStack...</p>
+                </div>
             </Link>
+
             {courses.map((c: Course) => (
                 <CourseButton key={c.id} course_id={c.id} course_name={c.name} icon={c.icon} />
             ))}
