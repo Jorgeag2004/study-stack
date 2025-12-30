0c35a47ef38febd8a72dfa80167fc7d748a92b19
Jorge Gonzalez <jorgeag2004@icloud.com>
2025-12-29 20:32:33 -0500

    fixed icon type error

--- src/components/CourseContent.tsx
+++ src/components/CourseContent.tsx
@@ -14,6 +14,7 @@ import {ContextMenu} from "@/components/buttons/ContextMenu";
 import {CourseForm} from "@/components/forms/CourseForm";
 import {delete_course_by_id} from "@/lib/data/delete_course_by_id";
 import * as LucideIcons from "lucide-react";
+import { LucideIcon} from "lucide-react";
 
 interface CourseContentProps {
     assignments: Assignment[],
@@ -31,7 +32,7 @@ export const CourseContent = ({assignments, study_items, learn_items, course_id,
     const [openAddLearnItem, setOpenAddLearnItem] = useState<boolean>(false);
     const [openEditCourse, setOpenEditCourse] = useState<boolean>(false);
 
-    const Icon = LucideIcons[icon] as React.FC<React.SVGProps<SVGSVGElement>>;
+    const Icon = (LucideIcons as unknown as Record<string, React.ComponentType>)[icon];
 
     return (
         <div className={'bg-neutral-800 w-full h-full'}>
